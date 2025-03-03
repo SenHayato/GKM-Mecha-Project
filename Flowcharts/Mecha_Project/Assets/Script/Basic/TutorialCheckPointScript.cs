8545036e4ee411a45dfd936637d5acf6f25776ff
SenHayato <157812043+SenHayato@users.noreply.github.com>
2025-03-03 14:59:14 +0700

    Update FIx Checkpoint

--- Flowcharts/Mecha_Project/Assets/Script/Basic/TutorialCheckPointScript.cs
+++ Flowcharts/Mecha_Project/Assets/Script/Basic/TutorialCheckPointScript.cs
@@ -8,8 +8,13 @@ public class TurotialCheckPointScript : MonoBehaviour
     [SerializeField] private CheckPointNumber checkPointNumber;
     [SerializeField] private string checkPointInfo;
     [SerializeField] private GameObject nextChekpoint;
-    [SerializeField] private float checkPointDuration;
+    [SerializeField] private float checkPointDuration; //Destroy Duration
     [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private Collider pointCollider;
+    //[SerializeField] private Material pointMaterial;
+
+    //Checker
+    GameObject[] enemies;
 
     private void Awake()
     {
@@ -19,6 +24,22 @@ public class TurotialCheckPointScript : MonoBehaviour
     private void Start()
     {
         meshRenderer = GetComponent<MeshRenderer>();
+        pointCollider = GetComponent<Collider>();
+    }
+
+    public void EnemyChecker()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (enemies.Length >= 1)
+        {
+            meshRenderer.enabled = false;
+            pointCollider.enabled = false;
+        }
+        else
+        {
+            meshRenderer.enabled = true;
+            pointCollider.enabled = true;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,16 +56,21 @@ public class TurotialCheckPointScript : MonoBehaviour
     {
         if (nextChekpoint != null)
         {
-             nextChekpoint.SetActive(true);
+            nextChekpoint.SetActive(true);
         }
         else
         {
             //nextStageTransition
         }
     }
+
+    private void Update()
+    {
+        EnemyChecker();
+    }
 }
 
 public enum CheckPointNumber
 {
-    CheckPoint1, CheckPoint2, CheckPoint3, CheckPoint4, CheckPoint5, CheckPoint6, CheckPoint7, CheckPoint8, CheckPoint9, CheckPoint10,
+    CheckPoint1, CheckPoint2, CheckPoint3, CheckPoint4, CheckPoint5, CheckPoint6, CheckPoint7
 }
