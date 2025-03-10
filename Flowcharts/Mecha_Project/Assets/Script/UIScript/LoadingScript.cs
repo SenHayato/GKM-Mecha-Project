7406e035f6644a5119f46088dce19b5c6701f0b7
SenHayato <157812043+SenHayato@users.noreply.github.com>
2025-03-10 15:28:04 +0700

    TerrainData

---
+++ Flowcharts/Mecha_Project/Assets/Script/UIScript/LoadingScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScript : MonoBehaviour
+{
+    [SerializeField] UnityEngine.UI.Slider progressBar;
+    [SerializeField] int progress;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
