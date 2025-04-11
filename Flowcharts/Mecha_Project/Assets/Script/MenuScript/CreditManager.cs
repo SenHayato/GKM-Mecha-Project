7ccfc2c19ba04c93ee54648ee30a4283cd8de44e
SenHayato <157812043+SenHayato@users.noreply.github.com>
2025-04-11 09:38:52 +0700

    Credit Screen Jadi

--- Flowcharts/Mecha_Project/Assets/Script/MenuScript/CreditManager.cs
+++ Flowcharts/Mecha_Project/Assets/Script/MenuScript/CreditManager.cs
@@ -7,13 +7,14 @@ public class CreditManager : MonoBehaviour
 {
     [Header("Reference SetUp")]
     [SerializeField] MenuMaster menuMaster;
-    [SerializeField] bool creditPlaying;
-    [SerializeField] bool isPressed;
+    [SerializeField] MainMenuScript mainMenuScript;
+    //[SerializeField] bool isPressed;
 
     [Header("Credit Properties")]
     [SerializeField] GameObject creditText;
     [SerializeField] float creditDuration;
     [SerializeField] float scrollSpeed;
+    [SerializeField] Transform defaultTextPosition;
     //[SerializeField] AudioSource creditSource;
 
     //flag
@@ -23,42 +24,54 @@ public class CreditManager : MonoBehaviour
     {
         //creditSource = GetComponent<AudioSource>();
         menuMaster = FindFirstObjectByType<MenuMaster>();
+        mainMenuScript = FindFirstObjectByType<MainMenuScript>();
     }
 
-    private void Start()
+    //private void Start()
+    //{
+    //    //creditDuration = creditSource.clip.length;
+    //}
+
+    private void OnEnable()
     {
-        //creditDuration = creditSource.clip.length;
-        creditPlaying = true;
-        isPressed = false;
+        //isPressed = false;
+        timeLerp = 0f;
+        creditText.transform.position = defaultTextPosition.position;
+        StartCoroutine(CreditPlaying());
     }
 
     IEnumerator CreditPlaying()
     {
-        if (creditPlaying)
+        while (timeLerp < 1f) // Selama belum mencapai durasi
         {
             timeLerp += Time.deltaTime / creditDuration;
-            creditText.transform.position = new Vector2 (transform.position.x, scrollSpeed * timeLerp);
-            yield return new WaitForSeconds(creditDuration);
-            creditPlaying = false;
-            menuMaster.mainmenuScreenActive = true;
-            menuMaster.creditScreenActive = false;
-        }
-    }
+            float y = Mathf.Lerp(
+                defaultTextPosition.position.y,
+                defaultTextPosition.position.y + scrollSpeed,
+                timeLerp
+            );
 
+            creditText.transform.position = new Vector2(defaultTextPosition.position.x, y);
+            yield return null;
+        }
+
+        // Setelah durasi berakhir
+        menuMaster.mainmenuScreenActive = true;
+        mainMenuScript.menuAudio.enabled = true;
+        menuMaster.creditScreenActive = false;
+    }
     void SkipCreditButton()
     {
-        if (!isPressed && (Keyboard.current.anyKey.wasPressedThisFrame || Gamepad.current != null && Gamepad.current.buttonEast.wasPressedThisFrame))
+        if (Keyboard.current.anyKey.wasPressedThisFrame || Gamepad.current != null && Gamepad.current.buttonEast.wasPressedThisFrame)
         {
-            isPressed = true;
-            creditPlaying = false;
             menuMaster.mainmenuScreenActive = true;
+            mainMenuScript.menuAudio.enabled = true;
             menuMaster.creditScreenActive = false;
         }
     }
 
     void Update()
     {
-        StartCoroutine(CreditPlaying());
         SkipCreditButton();
     }
 }
