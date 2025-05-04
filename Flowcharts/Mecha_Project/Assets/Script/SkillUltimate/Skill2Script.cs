586095702890754e863c2ef287f493c6340f6ac4
SenHayato <157812043+SenHayato@users.noreply.github.com>
2025-05-04 11:28:49 +0700

    Udah Fix
    skill dan ultimate berdamage
    player is death dimatikan untuk testing

--- Flowcharts/Mecha_Project/Assets/Script/SkillUltimate/Skill2Script.cs
+++ Flowcharts/Mecha_Project/Assets/Script/SkillUltimate/Skill2Script.cs
@@ -8,20 +8,33 @@ public class Skill2Script : MonoBehaviour
     [SerializeField] MechaPlayer playerData;
     [SerializeField] PlayerActive playerActive;
 
+    [SerializeField] Vector3 boxSize;
+
     private void Awake()
     {
         playerData = GetComponentInParent<MechaPlayer>();
         playerActive = GetComponentInParent<PlayerActive>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnEnable()
     {
-        if ((playerActive.enemyLayer.value & (1 << other.gameObject.layer)) != 0)
+        //buat collider manual dengan overlapbox
+        Collider[] hitColliders = Physics.OverlapBox(transform.position, boxSize / 2f, transform.rotation, playerActive.enemyLayer);
+
+        foreach (var hitCollider in hitColliders)
         {
-            if (other.TryGetComponent<EnemyActive>(out var enemy))
+            if (hitCollider.TryGetComponent<EnemyActive>(out var enemy))
             {
                 enemy.TakeDamage(playerData.skill2Damage);
             }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Visualisasi area box
+        Gizmos.color = Color.yellow;
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        Gizmos.DrawWireCube(Vector3.zero, boxSize);
     }
 }
