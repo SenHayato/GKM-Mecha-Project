89a138bf39787740078408151f4a7eeef2e9b38c
SenHayato <157812043+SenHayato@users.noreply.github.com>
2025-12-09 18:59:22 +0700

    Update

--- Flowcharts/Mecha_Project/Assets/Script/SkillUltimate/WeaponSkillScript.cs
+++ Flowcharts/Mecha_Project/Assets/Script/SkillUltimate/WeaponSkillScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class Skill2Script : MonoBehaviour
+public class WeaponScript : MonoBehaviour
 {
     [SerializeField] MechaPlayer playerData;
     [SerializeField] PlayerActive playerActive;
