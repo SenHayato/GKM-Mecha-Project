9ee85a6d35e39b919ef5fa67c01499172c1d7922
SenHayato <157812043+SenHayato@users.noreply.github.com>
2025-12-27 13:27:41 +0700

    Optimize

--- Flowcharts/Mecha_Project/Assets/Script/Enemy/ShortState/StunState.cs
+++ Flowcharts/Mecha_Project/Assets/Script/Enemy/ShortState/StunState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StuntState : StateMachineBehaviour
+public class StunState : StateMachineBehaviour
 {
     [SerializeField] EnemyActive enemyActive;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
